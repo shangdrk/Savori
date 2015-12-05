2379f6aab6f545dc7a87c3724354a3913eb28f9d
Derek Shang <derekshang07@gmail.com>
2015-12-06 00:23:06 +0100

    initial commit

---
+++ app/src/main/java/com/zoray/savori/adapters/MainFragmentPagerAdapter.java
@@ -0,0 +1,22 @@
+package com.zoray.savori.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public MainFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
