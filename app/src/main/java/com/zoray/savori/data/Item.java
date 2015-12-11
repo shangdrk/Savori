ad5c475743da5a5ac9fb8f6944e9a096b0add982
amandayin95 <amandayin95@gmail.com>
2015-12-11 19:39:55 +0100

    Adding all changed files Search

--- app/src/main/java/com/zoray/savori/data/Item.java
+++ app/src/main/java/com/zoray/savori/data/Item.java
@@ -4,4 +4,30 @@ package com.zoray.savori.data;
  * Created by amandayin on 12/10/15.
  */
 public class Item {
+
+    private String name;
+    private boolean bought;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBought(boolean bought) {
+        this.bought = bought;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public boolean isBought() {
+        return bought;
+    }
+
+    public Item(String name, boolean bought) {
+
+        this.name = name;
+        this.bought = bought;
+    }
 }
